aea9070dbc1dbabf17e2e0a9b1e5cd750f2954d6
puuu <puuu@users.noreply.github.com>
2017-03-22 16:08:41 +0900

    USI_TWI_Master.h: use usi_io.h

--- src/USI_TWI_Master/USI_TWI_Master.h
+++ src/USI_TWI_Master/USI_TWI_Master.h
@@ -73,47 +73,7 @@
 
 // Device dependant defines
 #if __GNUC__
-#if defined(__AVR_AT90Mega169__) || defined(__AVR_ATmega169PA__) || defined(__AVR_AT90Mega165__)                       \
-    || defined(__AVR_ATmega165__) || defined(__AVR_ATmega325__) || defined(__AVR_ATmega3250__)                         \
-    || defined(__AVR_ATmega645__) || defined(__AVR_ATmega6450__) || defined(__AVR_ATmega329__)                         \
-    || defined(__AVR_ATmega3290__) || defined(__AVR_ATmega649__) || defined(__AVR_ATmega6490__)
-#define DDR_USI DDRE
-#define PORT_USI PORTE
-#define PIN_USI PINE
-#define PORT_USI_SDA PORTE5
-#define PORT_USI_SCL PORTE4
-#define PIN_USI_SDA PINE5
-#define PIN_USI_SCL PINE4
-#endif
-#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_AT90Tiny26__) \
-    || defined(__AVR_ATtiny26__) || defined( __AVR_ATtiny261__ ) || defined( __AVR_ATtiny461__ )                       \
-    || defined( __AVR_ATtiny861__ )
-#define DDR_USI DDRB
-#define PORT_USI PORTB
-#define PIN_USI PINB
-#define PORT_USI_SDA PORTB0
-#define PORT_USI_SCL PORTB2
-#define PIN_USI_SDA PINB0
-#define PIN_USI_SCL PINB2
-#endif
-#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-#define DDR_USI DDRA
-#define PORT_USI PORTA
-#define PIN_USI PINA
-#define PORT_USI_SDA PORTA6
-#define PORT_USI_SCL PORTA4
-#define PIN_USI_SDA PINA6
-#define PIN_USI_SCL PINA4
-#endif
-#if defined(__AVR_AT90Tiny2313__) || defined(__AVR_ATtiny2313__) || defined(__AVR_ATtiny4313__)
-#define DDR_USI DDRB
-#define PORT_USI PORTB
-#define PIN_USI PINB
-#define PORT_USI_SDA PORTB5
-#define PORT_USI_SCL PORTB7
-#define PIN_USI_SDA PINB5
-#define PIN_USI_SCL PINB7
-#endif
+#include "usi_io.h"
 #else //__GNUC__
 #if defined(__AT90Mega169__) || defined(__ATmega169__) || defined(__AT90Mega165__) || defined(__ATmega165__)           \
     || defined(__ATmega325__) || defined(__ATmega3250__) || defined(__ATmega645__) || defined(__ATmega6450__)          \
